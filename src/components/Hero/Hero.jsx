407fc250c72bfd26e5519d59d2592e541705d3b9
mikolajmki <mikol4m@gmail.com>
2023-02-27 13:43:59 +0100

    sides

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import css from './Hero.module.css';
 import HeroImage from '../../assets/hero.png';
-import { BiShoppingBag } from 'react-icons/bi';
+import { RiShoppingBagFill } from 'react-icons/ri';
+import { BsArrowRight } from 'react-icons/bs';
 
 export const Hero = () => {
     return (
         <div className={css.container}>
             
-            <div className={css.leftSide}>
-                <span>skin protection cream</span>
+            <div className={css.side}>
+                <span className={css.text1}>skin protection cream</span>
 
                 <div className={css.text2}>
                     <span>Special collection</span>
@@ -20,14 +21,28 @@ export const Hero = () => {
                 <div className={css.blueCircle}></div>
                 <img className={css.heroImage} src={HeroImage} alt="" />
                 <div className={css.cart2}>
-                    <BiShoppingBag/>
+                    <RiShoppingBagFill/>
                     <div className={css.signup}>
                         <span>Best signup offers</span>
 
                         <div>
-                            {/* <BsArrowRight/> */}
+                            <BsArrowRight/>
                         </div>
                     </div>
+                </div>
+            </div>
+
+            <div className={css.side}>
+
+                <div className={css.traffic}>
+
+                    <span>100k</span>
+                    <span>Monthly traffic</span>
+                </div>
+
+                <div className={css.customers}>
+                    <span>150k</span>
+                    <span>Happy Customers</span>
                 </div>
             </div>
         </div>
